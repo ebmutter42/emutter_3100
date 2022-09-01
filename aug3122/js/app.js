53f4cad5b164ede00135024e16619ab98c90f529
ebmutter42 <88211836+ebmutter42@users.noreply.github.com>
2022-08-31 22:26:22 -0500

    08/31/22

--- aug3122/js/app.js
+++ aug3122/js/app.js
@@ -1,5 +1,5 @@
 $('#btnLogin').on('click', function(){
-    let strEmail = document.getElementById('#txtEmail').value();
+    let strEmail = document.getElementById('#txtEmail').val();
     let strPassword = document.getElementById('#txtPassword').val(); 
     console.log(strEmail);
     console.log(strPassword);
